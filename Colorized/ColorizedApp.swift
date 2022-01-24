d40579dc2058d8a70fc8324811a13732c36bf167
Pavel <92750573+Opt1mus26@users.noreply.github.com>
2022-01-24 22:36:52 +0300

    Initial Commit

---
+++ Colorized/ColorizedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp.swift
+//  Colorized
+//
+//  Created by Pavel Tsyganov on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
